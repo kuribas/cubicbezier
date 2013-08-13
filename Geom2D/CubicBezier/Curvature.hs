f11f651b1123dac91e71323c989698960fdebd67
Kristof Bastiaensen <kristof@vleeuwen.org>
2013-08-13 23:01:41 +0200

    Fixed the findRadius function, and removed compareRadius since it is unnecessary.

--- Geom2D/CubicBezier/Curvature.hs
+++ Geom2D/CubicBezier/Curvature.hs
@@ -5,20 +5,6 @@ import Geom2D.CubicBezier.Basic
 import Geom2D.CubicBezier.Numeric
 import Data.Maybe
 
--- | Compare a value with the radius of curvature.  This
--- is similar to @compare value radius@, but works with 
--- infinite radius.
-compareRadius :: CubicBezier -- ^ The bezier curve
-              -> Double      -- ^ The distance to the curve
-              -> Double      -- ^ The parameter to the bezier
-              -> Ordering    -- ^ The result
-compareRadius b dist =
-  let bd = evalBezierDerivs b
-  in \t -> let
-    (_: Point x' y': Point x'' y'': _) = bd t
-    in compare ((x'^2 + y'^2)^3)
-       ((dist * (x' * y'' - y' * x''))^2)
-
 -- | Curvature of the Bezier curve.
 curvature :: CubicBezier -> Double -> Double
 curvature b =
@@ -29,16 +15,16 @@ curvature b =
 
 -- | Radius of curvature of the Bezier curve.  This
 -- is the reciprocal of the curvature.
-radiusCurvature :: CubicBezier -> Double -> Double
-radiusCurvature b =
+radiusOfCurvature :: CubicBezier -> Double -> Double
+radiusOfCurvature b =
   let bd = evalBezierDerivs b
   in \t -> let
     (_: Point x' y': Point x'' y'': _) = bd t
     in (x'^2 + y'^2)**1.5 / (x' * y'' - y' * x'')
 
 -- | Find a local maximum or minimum of the curvature
--- on the bezier.  It doesn't return inflection points, and may
--- not find a solution when there are multiple local
+-- on the bezier.  It may not return all solutions
+-- when there are inflection points or multiple local
 -- extrema.
 
 -- calculate the extrama of the curvature by
@@ -53,11 +39,13 @@ curvatureExtremum b =
         in (y'^2 + x'^2) * (x'*y''' - y'*x''') - 3 * (x'*y'' - y'*x'') * (y'*y'' + x'*x'')
   in if signum (curvDeriv 0) == signum (curvDeriv 1)
      then Nothing -- we cannot use the root finder
-     else Just $ findRoot curvDeriv 1e-7 0 1
+     else Just $ findRoot curvDeriv 1e-8 0 1
+
+if' a b c = if a then b else c
 
 -- | Find points on the curve that have a certain radius of curvature.
--- This function may not find all points when there are more than two
--- such points, or the curve has inflection points.
+-- The curve shouldn't have inflection points.  This function may not
+-- find all points when there are more than two such points.
 findRadius b d = let
   bd = evalBezierDerivs b
   radiusSquare t =
@@ -65,14 +53,14 @@ findRadius b d = let
     in (x'^2 + y'^2)^3 - (d*(x'*y'' - y'*x''))^2
 
   radiusBetween t1 t2 =
-    let r1 = radiusCurvature b t1
-        r2 = radiusCurvature b t2
-        between | r1 < r2   = r1 <= d && d <= r2
-                | otherwise = r2 <= d && d <= r1
-    in if between
-       then [findRoot radiusSquare 1e-7 t1 t2]
-       else []
-
+    let r1 = radiusOfCurvature b t1
+        r2 = radiusOfCurvature b t2
+    in if' (r1 == d) [r1] $
+       if' (r2 == d) [r2] $
+       if' ((r1 * r2 >= 0) &&
+            (if r1 < r2 then r1 < d && d < r2 else r2 < d && d < r1))
+            [findRoot radiusSquare 1e-8 t1 t2]
+       []
   in case curvatureExtremum b of
     Nothing -> radiusBetween 0 1
     Just ex -> radiusBetween 0 ex ++ radiusBetween ex 1
