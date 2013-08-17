a4a629bc1f1a04cee07a71b7b974d2576c92cbac
Kristof Bastiaensen <kristof@vleeuwen.org>
2013-08-17 20:26:13 +0200

    Added intersection module to export list.

--- Geom2D/CubicBezier.hs
+++ Geom2D/CubicBezier.hs
@@ -3,13 +3,16 @@
 module Geom2D.CubicBezier
        (module Geom2D.CubicBezier.Basic,
         module Geom2D.CubicBezier.Approximate,
-        module Geom2D.CubicBezier.Outline
+        module Geom2D.CubicBezier.Outline,
+        module Geom2D.CubicBezier.Curvature,
+        module Geom2D.CubicBezier.Intersection
        ) where
 
 import Geom2D.CubicBezier.Basic
 import Geom2D.CubicBezier.Approximate
 import Geom2D.CubicBezier.Outline
 import Geom2D.CubicBezier.Curvature
+import Geom2D.CubicBezier.Intersection
        
        
         
