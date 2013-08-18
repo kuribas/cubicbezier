3df7ce97053b8c9518254feab2959de748a56fff
Kristof Bastiaensen <kristof@vleeuwen.org>
2013-08-18 10:59:10 +0200

    Some bug fixes in bezierIntersection.

--- Geom2D/CubicBezier/Intersection.hs
+++ Geom2D/CubicBezier/Intersection.hs
@@ -1,4 +1,6 @@
-module Geom2D.CubicBezier.Intersection (bezierIntersection)
+-- curve intersection using bezier clipping.
+module Geom2D.CubicBezier.Intersection
+       -- (bezierIntersection)
        where
 
 import Geom2D
@@ -16,12 +18,12 @@ project l = zipWith (flip (/)) [1..] $
             tail l
       
 -- find the outermost point.  It doesn't look at the x values.
--- The type is more scary than the function.  outerBy should
--- be maximumBy or minimumBy
+-- outerBy should be either maximumBy or minimumBy
 findOuter :: Ord a => (((a, b) -> (a, b) -> Ordering) ->
                        [(Double, [Point])] -> (c, [Point]))
              -> [Point] -> [Point]
-findOuter outerBy [] = []
+findOuter _ [] = []
+findOuter _ [p] = [p]
 findOuter outerBy l@(p:pts) = p : findOuter outerBy next
   where next = snd $ outerBy (compare `on` fst) $
                zip (project l) $ tails pts
@@ -34,43 +36,50 @@ makeHull ds =
   in (findOuter maximumBy points,
       findOuter minimumBy points)
 
--- test if the chords cross the line y=dmin somewhere
-testBelow dmin [] = Nothing
-testBelow dmin [_] = Nothing
-testBelow dmin (p:q:rest)
-  | pointY p >= dmin = Nothing
+-- test if the chords cross the fat line
+-- use continuation passing style
+testBelow :: Double -> [Point] -> Maybe Double -> Maybe Double
+testBelow dmin [] _ = Nothing
+testBelow dmin [_] _ = Nothing
+testBelow dmin (p:q:rest) cont
+  | pointY p >= dmin = cont
   | pointY p > pointY q = Nothing
-  | pointY q > dmin = testBelow dmin (q:rest)
+  | pointY q < dmin = testBelow dmin (q:rest) cont
   | otherwise = Just $ intersectPt dmin p q
 
+testBetween :: Double -> Point -> Maybe Double -> Maybe Double
+testBetween dmax (Point x y) cont
+  | (y <= dmax) = Just x
+  | otherwise = cont
+
 -- test if the chords cross the line y=dmax somewhere
+testAbove :: Double -> [Point] -> Maybe Double
 testAbove dmax [] = Nothing
 testAbove dmax [_] = Nothing
 testAbove dmax (p:q:rest)
-  | pointY p <= dmax = Nothing
   | pointY p < pointY q = Nothing
-  | pointY q < dmax = testBelow dmax (q:rest)
+  | pointY q > dmax = testAbove dmax (q:rest)
   | otherwise = Just $ intersectPt dmax p q
-
+                
 -- find the x value where the line through the two points
 -- intersect the line y=d
 intersectPt d (Point x1 y1) (Point x2 y2) =
   x1 + (d  - y1) * (x2 - x1) / (y2 - y1)
 
-chopHull dmin dmax ds = (left_t, right_t)
-  where
-    (upper, lower) = makeHull ds
-    left_t  = fromMaybe 0 $
-              testBelow dmin lower `mplus`
-              testAbove dmax upper
-    right_t = fromMaybe 1 $
-              (testBelow dmin $ reverse lower) `mplus`
-              (testAbove dmax $ reverse upper)
+chopHull dmin dmax ds = do
+  let (upper, lower) = makeHull ds
+  left_t <- testBelow dmin upper $
+            testBetween dmax (head upper) $
+            testAbove dmax lower
+  right_t <- testBelow dmin (reverse upper) $
+             testBetween dmax (last upper) $
+             testAbove dmax (reverse lower)
+  Just (left_t, right_t)
 
-fatLineDistance (Point x1 y1) (Point x2 y2) = \(Point x y) -> a*x + b*y + c
+lineDistance (Point x1 y1) (Point x2 y2) = \(Point x y) -> a*x + b*y + c
   where a = a' / d
         b = b' / d
-        c = -(b' / a') / d
+        c = -(y1*b' + x1*a') / d
         a' = y1 - y2
         b' = x2 - x1
         d = sqrt(a'^2 + b'^2)
@@ -78,45 +87,53 @@ fatLineDistance (Point x1 y1) (Point x2 y2) = \(Point x y) -> a*x + b*y + c
 bezierClip p@(CubicBezier p0 p1 p2 p3) q@(CubicBezier q0 q1 q2 q3)
   tmin tmax umin umax prevClip eps reverse
 
+  -- no intersection
+  | chop_interval == Nothing = []
+
   -- not enough reduction, so split the curve in case we have
   -- multiple intersections
   | prevClip > 0.8 && newClip > 0.8 =
     if new_tmax - new_tmin > umax - umin -- split the longest segment
     then let
       (p1, p2) = splitBezier newP 0.5
-      half_t = (new_tmax-new_tmin) / 2
-      in bezierClip p1 q new_tmin half_t umin umax newClip eps reverse ++
-         bezierClip p2 q half_t tmax umin umax newClip eps reverse
+      half_t = new_tmin + (new_tmax - new_tmin) / 2
+      in bezierClip q p1 umin umax new_tmin half_t newClip eps (not reverse) ++
+         bezierClip q p2 umin umax half_t new_tmax newClip eps (not reverse)
     else let
       (q1, q2) = splitBezier q 0.5
-      half_t = (umax - umin) / 2
-      in bezierClip newP q1 new_tmin new_tmax umin half_t newClip eps reverse ++
-         bezierClip newP q2 new_tmin new_tmax half_t umax newClip eps reverse
+      half_t = umin + (umax - umin) / 2
+      in bezierClip q1 newP umin half_t new_tmin new_tmax newClip eps (not reverse) ++
+         bezierClip q2 newP half_t umax new_tmin new_tmax newClip eps (not reverse)
 
   -- within tolerance      
   | max (umax - umin) (new_tmax - new_tmin) < eps =
     if reverse
-    then [(umax - umin)/2, (new_tmax/new_tmin)/2]
-    else [(new_tmax - new_tmin)/2 , (umax - umin)/2]
+    then [(q, p, umin, umax, new_tmin, new_tmax)]
+    else [(p, q, new_tmin, new_tmax, umin, umax)]
 
   -- iterate with the curves reversed.
   | otherwise =
-      bezierClip q p umin umax tmin tmax newClip eps (not reverse)
+      bezierClip q newP umin umax new_tmin new_tmax newClip eps (not reverse)
 
   where
-    d = fatLineDistance q0 q3
+    d = lineDistance q0 q3
     d1 = d q1
     d2 = d q2
     (dmin, dmax) | d1*d2 > 0 = (3/4 * minimum [0, d1, d2],
                                 3/4 * maximum [0, d1, d2])
                  | otherwise = (4/9 * minimum [0, d1, d2],
                                 4/9 * maximum [0, d1, d2])
-    (chop_tmin, chop_tmax) = chopHull dmin dmax $ map d [p0, p1, p2, p3]
-    newP = fst $ flip splitBezier chop_tmin $
-           snd $ splitBezier p chop_tmax
+    chop_interval = chopHull dmin dmax $
+                    map d [p0, p1, p2, p3]
+    Just (chop_tmin, chop_tmax) = chop_interval
+    newP = snd $ flip splitBezier chop_tmin $
+           fst $ splitBezier p chop_tmax
     newClip = chop_tmax - chop_tmin
-    new_tmin = chop_tmin * (tmax - tmin) + tmin
-    new_tmax = chop_tmax * (tmax - tmin) + tmin
+    new_tmin = tmax * chop_tmin + tmin * (1 - chop_tmin)
+    new_tmax = tmax * chop_tmax + tmin * (1 - chop_tmax)
 
 -- | Find the intersections between two Bezier curves to within tolerance eps.
-bezierIntersection p q eps = bezierClip p q 0 1 0 1 1 eps False
+bezierIntersection p q eps = bezierClip p q 0 1 0 1 0 eps False
+
+b1 = CubicBezier (Point 1 1) (Point 2 4) (Point 3 4) (Point 4 3)
+b2 = CubicBezier (Point 3 5) (Point 3 6) (Point 0 1) (Point 3 1)
