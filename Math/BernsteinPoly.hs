cabb34f0162a17cc02765ef93817c9986251cdf3
Kristof Bastiaensen <kristof@vleeuwen.org>
2013-09-15 14:09:00 +0200

    Unrolled a definition for performance.

--- Math/BernsteinPoly.hs
+++ Math/BernsteinPoly.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Math.BernsteinPoly
        (BernsteinPoly(..), bernsteinSubsegment, listToBernstein, zeroPoly, (~*), (*~), (~+),
         (~-), degreeElevate, bernsteinSplit, bernsteinEval,
@@ -7,8 +8,8 @@ module Math.BernsteinPoly
 import Data.List
 
 data BernsteinPoly = BernsteinPoly {
-  bernsteinDegree :: Int,
-  bernsteinCoeffs :: [Double] }
+  bernsteinDegree :: !Int,
+  bernsteinCoeffs :: ![Double] }
                    deriving Show
 
 infixl 7 ~*, *~
@@ -49,32 +50,38 @@ bernsteinSubsegment b t1 t2
         a' = zipWith (*) a (binCoeff la)
         b' = zipWith (*) b (binCoeff lb)
 
-degreeElevate' :: BernsteinPoly -> Int -> BernsteinPoly
-degreeElevate' b 0 = b
-degreeElevate' (BernsteinPoly lp p) times =
-  degreeElevate' (BernsteinPoly (lp+1) (head p:inner p 1)) (times-1)
-  where
-    inner [a] _ = [a]
-    inner (a:b:rest) i =
-      (i*a/fromIntegral lp + b*(1 - i/fromIntegral lp))
-      : inner (b:rest) (i+1)
 
 -- find the binomial coefficients of degree n.
 binCoeff :: Int -> [Double]
 binCoeff n = map fromIntegral $
              scanl (\x m -> x * (n-m+1) `quot` m) 1 [1..n]
 
--- | Degree elevate a bernstein polynomail.
+-- | Degree elevate a bernstein polynomail a number of times.
 degreeElevate :: BernsteinPoly -> Int -> BernsteinPoly
-degreeElevate l times = degreeElevate' l times
+degreeElevate b 0 = b
+degreeElevate (BernsteinPoly lp p) times =
+  degreeElevate (BernsteinPoly (lp+1) (head p:inner p 1)) (times-1)
+  where
+    inner []  _ = error "empty bernstein coefficients"
+    inner [a] _ = [a]
+    inner (a:b:rest) i =
+      (i*a/fromIntegral lp + b*(1 - i/fromIntegral lp))
+      : inner (b:rest) (i+1)
+
 
 -- | Evaluate the bernstein polynomial.
 bernsteinEval :: BernsteinPoly -> Double -> Double
-bernsteinEval (BernsteinPoly lp p) t = foldl' addcoeff 0 $
-                                       zip3 ts (binCoeff lp) p
-  where ts = iterate (*t) 1
-        u = 1-t
-        addcoeff a (s, d, b) = (a*u + b*s*d)
+bernsteinEval (BernsteinPoly lp [b]) _ = b
+bernsteinEval (BernsteinPoly lp (b':bs)) t = go t n (b'*u) 2 bs
+  where u = 1-t
+        n = fromIntegral lp
+        go !tn !bc !tmp _  [b] = tmp + tn*bc*b
+        go !tn !bc !tmp !i (b:rest) =
+          go (tn*t)         -- tn
+          (bc*(n-i+1)/i)    -- bc
+          ((tmp + tn*bc*b)*u) -- tmp
+          (i+1)             -- i
+          rest
 
 -- | Evaluate the bernstein polynomial and its derivatives.
 bernsteinEvalDerivs :: BernsteinPoly -> Double -> [Double]
@@ -88,8 +95,7 @@ bernsteinDeriv :: BernsteinPoly -> BernsteinPoly
 bernsteinDeriv (BernsteinPoly 0 _) = zeroPoly
 bernsteinDeriv (BernsteinPoly lp p) =
   BernsteinPoly (lp-1) $
-  map (* fromIntegral lp) $
-  zipWith (-) (tail p) p
+  map (* fromIntegral lp) $ zipWith (-) (tail p) p
 
 -- | Split a bernstein polynomial
 bernsteinSplit :: BernsteinPoly -> Double -> (BernsteinPoly, BernsteinPoly)
