cabb17b945350c447685abe556114dd15ef9a8e8
Kristof Bastiaensen <kristof@vleeuwen.org>
2013-09-15 14:51:05 +0200

    Geom2D will be exported also.

--- Geom2D/CubicBezier.hs
+++ Geom2D/CubicBezier.hs
@@ -1,13 +1,15 @@
--- | Export all the cubic bezier functions, but not the numeric helper functions
+-- | Export all the cubic bezier functions.
 
 module Geom2D.CubicBezier
        (module Geom2D.CubicBezier.Basic,
         module Geom2D.CubicBezier.Approximate,
         module Geom2D.CubicBezier.Outline,
         module Geom2D.CubicBezier.Curvature,
-        module Geom2D.CubicBezier.Intersection
+        module Geom2D.CubicBezier.Intersection,
+        module Geom2D
        ) where
 
+import Geom2D
 import Geom2D.CubicBezier.Basic
 import Geom2D.CubicBezier.Approximate
 import Geom2D.CubicBezier.Outline
